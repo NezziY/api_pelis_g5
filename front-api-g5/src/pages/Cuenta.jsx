ae5e567c519df00de5fbc67bbdcbcc7a3341ef07
Diego <dienqn84@gmail.com>
2024-07-08 12:13:12 -0300

    agrego contenido a mi cuenta

--- front-api-g5/src/pages/Cuenta.jsx
+++ front-api-g5/src/pages/Cuenta.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function Cuenta() {
   return (
-    <div>Cuenta</div>
-  )
+    <>
+      <div className="ml-auto mr-auto mt-7 w-96 backdrop-blur-lg bg-opacity-80 rounded-lg shadow-lg p-5 bg-gray-900 text-white">
+        <h2 className="text-2xl font-bold pb-5">Iniciar sesión</h2>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-2 text-sm font-medium">
+              Tu email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
+              placeholder="andrew@mail.com"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium"
+            >
+              Tu contraseña
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
+              placeholder="*********"
+              required
+            />
+          </div>
+
+          <div>
+            <p className="text-red-500 pb-5"></p>
+          </div>
+          <div className="flex flex-col items-center justify-between mb-4">
+            <button
+              type="submit"
+              className="text-white bg-purple-600 hover:bg-purple-700 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-sm py-2.5 px-5 w-full"
+            >
+              Conectarse
+            </button>
+            <div className="flex items-center text-sm mt-3">
+              <p>Quieres crear una cuenta?</p>
+              <Link to={`/registro/`} className="underline cursor-pointer ml-1">
+                Registrarse
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default Cuenta
+export default Cuenta;
--- front-api-g5/src/pages/Registro.jsx
+++ front-api-g5/src/pages/Registro.jsx
@@ -1,9 +1,98 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 function registro() {
   return (
-    <div>registro</div>
-  )
+    <>
+      <div className="mt-7 ml-auto mr-auto w-96 backdrop-blur-lg bg-opacity-80 rounded-lg shadow-lg p-5 bg-gray-900 text-white">
+        <h2 className="text-2xl font-bold pb-5">Registrarse</h2>
+        <form>
+          <div className="mb-4">
+            <label htmlFor="name" className="block mb-2 text-sm font-medium">
+              Nombre
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
+              placeholder="Nombre"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="name" className="block mb-2 text-sm font-medium">
+              Apellido
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
+              placeholder="Apellido"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-2 text-sm font-medium">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
+              placeholder="ejemplo@tumail.com"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium"
+            >
+              Contraseña
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
+              placeholder="*********"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium"
+            >
+              Repite tu contraseña
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 w-full py-2.5 px-4"
+              placeholder="*********"
+              required
+            />
+          </div>
+          <div>
+            <p className="text-red-500 pb-5"></p>
+          </div>
+          <div className="flex flex-col items-center justify-between mb-4">
+            <button
+              type="submit"
+              className="text-white bg-purple-600 hover:bg-purple-700 focus:ring-2 focus:ring-blue-300 font-medium rounded-lg text-sm py-2.5 px-5 w-full"
+            >
+              Registrarme
+            </button>
+            <div className="flex items-center text-sm mt-3">
+              <p>Ya tienes una cuenta?</p>
+              <Link to={`/cuenta`} className="underline cursor-pointer ml-1">
+                Iniciar sesión
+              </Link>
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default registro
+export default registro;
